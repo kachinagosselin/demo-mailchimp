c0538195d0266992304154207c8a3f2127f7e4fe
Kachina Gosselin <kachina@alum.mit.edu>
2016-08-11 14:37:58 -0700

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise  :database_authenticatable, :registerable, 
+         :recoverable, :rememberable, :trackable #:invitable, :confirmable,:validatable
+
+  after_create :add_mailchimp_subscriber
+
+private
+  def add_mailchimp_subscriber
+    client = Mailchimp::API.new('1b7b418d6647063fd6b864ada139236c-us14')
+    client.lists.subscribe('b46d2081e0', {email: self.email})
+  end
+
 end
